4a77e92cedfaba1dbe2884efb695b9b940778ffc
Heart-travel <zcgacbc@gmail.com>
2016-01-03 22:05:20 +0800

    Success in opening Bluetooth

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.helloworld;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,12 +18,21 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 private EditText editText;
+private BluetoothAdapter mBluetoothAdapter = null;
+private static final String TAG = "BluetoothChat";
+private static final boolean D = true;
 	
+//Intent request codes
+//private static final int REQUEST_CONNECT_DEVICE = 1;
+private static final int REQUEST_ENABLE_BT = 2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		editText = (EditText) findViewById(R.id.edit_text);
+		// Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		
 		//»ñÈ¡CheckBoxÊµÀý
 		CheckBox vib = (CheckBox)this.findViewById(R.id.vib);
@@ -46,6 +58,22 @@ private EditText editText;
 		});
 	}
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(D) Log.e(TAG, "++ ON START ++");
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        // Otherwise, setup the chat session
+        /*} else {
+            if (mChatService == null) setupChat();*/
+        }
+    }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
